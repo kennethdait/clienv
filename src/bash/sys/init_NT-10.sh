370c35ad0dae60d97d7ddef63ef2475e23407f63
Kenneth Dait <kendaitdev@icloud.com>
2021-03-11 06:35:19 -0600

    winproc: broke out base function to get task list

--- src/bash/sys/init_NT-10.sh
+++ src/bash/sys/init_NT-10.sh
@@ -10,9 +10,12 @@ fi
 
 winproc () 
 { 
-    while IFS=' 
-' read -r NAME ID GROUP XX SIZE; do
-        printf '\t%50s: !%-10s %s\n' "${NAME}" "${ID}" "${SIZE}";
-    done < <(tasklist | tr '\t' ' ' | tr -s ' ' | sed -E -e 's/[[^::blank:]]*(.*)[[:blank:]]*$/\1/g' | sed 's/ K$/K/' | sed -E -e 's/,([0-9]+)/\1/');
-    return
+	function get_task_list() {
+		tasklist | tr '\t' ' ' | tr -s ' ' | sed -E -e 's/[[^::blank:]]*(.*)[[:blank:]]*$/\1/g' | sed 's/ K$/K/' | sed -E -e 's/,([0-9]+)/\1/'
+		return
+	}
+	while IFS=$' \n' read -r NAME ID GROUP XX SIZE; do
+			printf '\t%50s: !%-10s %s\n' "${NAME}" "${ID}" "${SIZE}";
+	done < <(get_task_list);
+	return
 }
