7f2427604906c480fd3d5759d321c9076d72be62
Kenneth Dait <kendaitdev@icloud.com>
2021-03-10 23:56:07 -0600

    bash: added dependency checking, all sys

--- src/bash/sys/init_ALL.sh
+++ src/bash/sys/init_ALL.sh
@@ -1,3 +1,21 @@
 #!/usr/bin/env bash
 # sys/init_ALL.sh
 
+if
+	! which "${CLIENV_DEPS[@]}" &> /dev/null
+then
+	printf '\n\t[clienv|[31;1mWARN[00m]: %s\n' \
+		"some dependencies were not met"
+	declare OUTPUT
+	OUTPUT=''
+	for NAME in "${CLIENV_DEPS[@]}"; do
+		if ! which "${NAME}" &> /dev/null; then
+			OUTPUT+="\\t   [[31;1mmissing[00m]: ${NAME}\\n"
+		fi
+	done
+	[[ -z "${OUTPUT:-}" ]] \
+		&& OUTPUT="\\t  [UNK ERR]\\n"
+	echo -e "${OUTPUT}"
+	unset OUTPUT
+fi
+
