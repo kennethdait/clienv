4094a6ccf5a810b0097f2798b8e8f2a8856dbc36
Kenneth Dait <kendaitdev@icloud.com>
2021-03-10 23:32:37 -0600

    bash: added node.js to windows path
    * also fixed exported location vars

--- src/bash/sys/init_NT-10.sh
+++ src/bash/sys/init_NT-10.sh
@@ -2,3 +2,7 @@
 # sys/init_NT-10.sh
 
 printf '\n\t[clienv]: %s\n\n' "\$clienv/src/bash/sys/init_NT-10.sh sourced..."
+
+if [[ "${TERM_PROGRAM}" == 'mintty' ]]; then
+	export PATH=/c/nodejs"${PATH:+":${PATH}"}"
+fi
