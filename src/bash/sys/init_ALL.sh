e705c91838189960664cfb8b32d917cd69354b62
Kenneth Dait <kendaitdev@icloud.com>
2021-03-11 12:54:51 -0600

    bash: `list` function
    * lists file contents of a directory
    * lists pip packages by passing in `pip` in $1

--- src/bash/sys/init_ALL.sh
+++ src/bash/sys/init_ALL.sh
@@ -19,3 +19,42 @@ then
 	unset OUTPUT
 fi
 
+function list () {
+
+	function list_dir_contents_x1 () {
+		printf '\n\t[40;33;1m CONTENTS OF: [00m[43;30;1m %s [00m\n\n' "${TARGET}" \
+			&& find "${TARGET}" -mindepth 1 -maxdepth 1 \
+				| sed -E -e 's:^.*/::' \
+				| sed -E -e '/^\./d' \
+				| sort \
+				| column \
+			&& echo;
+		return
+	}
+
+	function list_pip_packages () {
+		/usr/bin/printf '\n\t[43;34;1m PIP LIST: [00m\n\n' \
+			&& pip list \
+				| /usr/bin/sed '1,2d' \
+				| /usr/bin/tr '\t' ' ' \
+				| /usr/bin/tr -s ' ' \
+				| /usr/bin/sed 's/[ ].*$//' \
+				| /usr/bin/sort -df \
+				| /usr/bin/column \
+			&& builtin echo
+		return
+	}
+
+	if [[ -n "${1:-}" ]]; then
+		if [[ "${1}" == 'pip' ]]; then
+			list_pip_packages
+			return
+		fi
+	fi
+
+	local -r TARGET="${1:-"${PWD}"}";
+
+	list_dir_contents_x1
+	return
+}
+
