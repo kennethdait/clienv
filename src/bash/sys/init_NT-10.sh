f7b395e80a147773bf332be16dfe121f9dbaefd9
Kenneth Dait <kendaitdev@icloud.com>
2021-03-11 06:33:35 -0600

    winproc: format spacing

--- src/bash/sys/init_NT-10.sh
+++ src/bash/sys/init_NT-10.sh
@@ -12,7 +12,7 @@ winproc ()
 { 
     while IFS=' 
 ' read -r NAME ID GROUP XX SIZE; do
-        printf '\t[%s]: [%s] %s\n' "${NAME}" "${ID}" "${SIZE}";
+        printf '\t%50s: !%-10s %s\n' "${NAME}" "${ID}" "${SIZE}";
     done < <(tasklist | tr '\t' ' ' | tr -s ' ' | sed -E -e 's/[[^::blank:]]*(.*)[[:blank:]]*$/\1/g' | sed 's/ K$/K/' | sed -E -e 's/,([0-9]+)/\1/');
     return
 }
