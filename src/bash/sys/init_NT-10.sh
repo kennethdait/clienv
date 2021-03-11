6ff539f4238435e072f243a4160b27aad10fd66e
Kenneth Dait <kendaitdev@icloud.com>
2021-03-11 06:12:58 -0600

    moved `winproc` to windows *NT* init file

--- src/bash/sys/init_NT-10.sh
+++ src/bash/sys/init_NT-10.sh
@@ -6,3 +6,13 @@ printf '\n\t[clienv]: %s\n\n' "\$clienv/src/bash/sys/init_NT-10.sh sourced..."
 if [[ "${TERM_PROGRAM}" == 'mintty' ]]; then
 	export PATH=/c/nodejs"${PATH:+":${PATH}"}"
 fi
+
+
+winproc () 
+{ 
+    while IFS=' 
+' read -r NAME ID GROUP XX SIZE; do
+        printf '\t[%s]: [%s] %s\n' "${NAME}" "${ID}" "${SIZE}";
+    done < <(tasklist | tr '\t' ' ' | tr -s ' ' | sed -E -e 's/[[^::blank:]]*(.*)[[:blank:]]*$/\1/g' | sed 's/ K$/K/' | sed -E -e 's/,([0-9]+)/\1/');
+    return
+}
