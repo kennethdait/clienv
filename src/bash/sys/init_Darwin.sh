c48d1ad469844909da4c420db158315ac5e62777
Kenneth Dait <kendaitdev@icloud.com>
2021-03-11 12:43:34 -0600

    bash-darwin: installed brew python executables to path
    * this installs:
      * python3 as `python`
      * pip3 as `pip`

--- src/bash/sys/init_Darwin.sh
+++ src/bash/sys/init_Darwin.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 # sys/init_Darwin.sh
 
-printf '\n\t[clienv]: %s\n\n' "\$clienv/src/bash/sys/init_Darwin.sh sourced..."
+#printf '\n\t[clienv]: %s\n\n' "\$clienv/src/bash/sys/init_Darwin.sh sourced..."
+
+if ! echo "${PATH}" | grep -Eq -e '/usr/local/opt/python/libexec/bin'; then
+	export PATH=/usr/local/opt/python/libexec/bin"${PATH:+":${PATH}"}"
+fi
+
