e0af8fe6c1913a7d5794453436506410f07b50c2
Kenneth Dait <kendaitdev@icloud.com>
2021-03-10 23:19:48 -0600

    bash: sys init files echo line

--- src/bash/sys/init_Linux.sh
+++ src/bash/sys/init_Linux.sh
@@ -1,2 +1,4 @@
-#/usr/bin/env bash
+#!/usr/bin/env bash
 # sys/init_Linux.sh
+
+printf '\n\t[clienv]: %s\n\n' "\$clienv/src/bash/sys/init_Linux.sh sourced..."
--- src/bash/sys/init_NT-10.sh
+++ src/bash/sys/init_NT-10.sh
@@ -1,2 +1,4 @@
-#/usr/bin/env bash
+#!/usr/bin/env bash
 # sys/init_NT-10.sh
+
+printf '\n\t[clienv]: %s\n\n' "\$clienv/src/bash/sys/init_NT-10.sh sourced..."
